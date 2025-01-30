38238e7da26c2393dfeb1bd4979a997c3a801257
Dharav <dharav9shah@gmail.com>
2025-01-29 22:27:59 -0500

    changes

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Player.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Player.java
@@ -1,61 +1,81 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 public class Player {
-    public int x, y;    // Player's position
-    private int direction; // 0 = Up, 1 = Right, 2 = Down, 3 = Left
-    public String path = "";
-    protected final int[] dy = {-1, 0, 1, 0}; // x-direction changes for Up, Right, Down, Left
-    protected final int[] dx = {0, 1, 0, -1};
-    public boolean doneMaze = false;
+    //public int x, y;   //position in maze
+    //private int direction; //direction facing 0 = North, 1 = East, 2 = south, 3 = West
+    public Path path;
+    public Algorithm stratagy;
+    //protected final int[] dy = {-1, 0, 1, 0}; 
+    //protected final int[] dx = {0, 1, 0, -1};
+    //public boolean doneMaze = false;
 
 
-    private Maze maze;
-    //Algorithm algorithm;
+    //private final Maze maze;
 
-    public Player(Maze maze) { //Algorithm algorithm taken out for now
-        this.maze = maze;
-        this.x = maze.getStartX();
-        this.y = maze.getStartY();
-        this.direction = 1; // Starting direction (right)
-        //this.algorithm = new Algorithm(maze);
+    public Player() {
+        this.path = new Path();
+        //this.maze = maze;
+        //this.x = maze.getStartX();
+        //this.y = maze.getStartY();
     }
 
-    public void followMove(String move) {
-            switch (move) {
-                case "F":
-                    moveForward();
-                    break;
-                case "L":
-                    turnLeft();
-                    break;
-                case "R":
-                    turnRight();
-                    break;
-            }
+    // public void followMove(String move) {
+    //         switch (move) {
+    //             case "F":
+    //                 moveForward();
+    //                 break;
+    //             case "L":
+    //                 turnLeft();
+    //                 break;
+    //             case "R":
+    //                 turnRight();
+    //                 break;
+    //             case "B":
+    //                 turnBack();
+    //                 break;
+    //         }
+    // }
+
+    // private void moveForward() {
+    //     //x += dx[direction];
+    //     //y += dy[direction];
+    //     path += "F";
+    //     //isAtEnd();
+    // }
+
+    // private void turnLeft() {
+    //     //direction = (direction + 3) % 4; // Turn counter-clockwise
+    //     path += "L";
+    //     //isAtEnd();
+    // }
+
+    // private void turnRight() {
+    //     //direction = (direction + 1) % 4; // Turn clockwise
+    //     path += "R";
+    //     //isAtEnd();
+    // }
+
+    // public void turnBack(){
+    //     //direction = (direction + 2) % 4; // Turn clockwise
+    //     path += "B";
+    //     //isAtEnd();
+    // }
+
+    // public void isAtEnd() {
+    //     if (x == maze.getEndX() && y == maze.getEndY()){
+    //         doneMaze = true;
+    //     }
+    // }
+
+    public void get_Stratagy(Algorithm algorithm){
+        this.stratagy = algorithm;
     }
 
-    private void moveForward() {
-        x += dx[direction];
-        y += dy[direction];
-        path += "F";
-        isAtEnd();
+    public void get_path(){
+        this.path.pathway = String.join("", this.stratagy.findPath());
     }
 
-    private void turnLeft() {
-        direction = (direction + 3) % 4; // Turn counter-clockwise
-        path += "L";
-        isAtEnd();
-    }
-
-    private void turnRight() {
-        direction = (direction + 1) % 4; // Turn clockwise
-        path += "R";
-        isAtEnd();
-    }
-
-    public void isAtEnd() {
-        if (x == maze.getEndX() && y == maze.getEndY()){
-            doneMaze = true;
-        }
+    public boolean verrify(String path){
+        return(this.stratagy.validatePath(path));
     }
 }
